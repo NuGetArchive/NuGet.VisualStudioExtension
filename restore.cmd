e07a010dd660788c894a225049f48a458135491b
Justin Emgarten <justin@emgarten.com>
2015-06-01 20:48:11 -0700

    build

--- restore.cmd
+++ restore.cmd
@@ -1,2 +1,4 @@
 @echo off
 %teamcity.tool.NuGet.CommandLine.2.8.5.nupkg%\tools\nuget.exe restore NuGet.VisualStudioExtension.sln -source %env.NUGET_PUSH_TARGET%;https://www.nuget.org/api/v2
+
+build.cmd
