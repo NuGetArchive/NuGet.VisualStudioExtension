5f05ecb943d9c79287d3f44fa70330b384f26613
deepakaravindr <daravind@microsoft.com>
2015-06-15 17:47:48 -0700

    Use PumpingJTF in case of VsPackageUninstaller too, just like, VsPackageInstaller
    This fixes the (consistent) hang observed in the case of VsPackageUninstaller
    in case of the following repro steps
    
    1. Open VS that has installed Office Developoer Tools.
    2. Create an Apps for SharePoint of hosting type Provider hosted.
    3. Goto property page and clik on SharePoint node.
    2. Retarget the app to ShrePoint 2013 / SharePoint Online.
    
    Actual:
    Retargetting window appears and VS hangs.
    
    Expected:
    Retargetting should complete.

--- src/NuGet.VisualStudio.Implementation/Extensibility/VsPackageUninstaller.cs
+++ src/NuGet.VisualStudio.Implementation/Extensibility/VsPackageUninstaller.cs
@@ -7,6 +7,7 @@ using System.Globalization;
 using System.Threading;
 using EnvDTE;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Threading;
 using NuGet.Configuration;
 using NuGet.PackageManagement;
 using NuGet.ProjectManagement;
@@ -21,12 +22,15 @@ namespace NuGet.VisualStudio
         private ISettings _settings;
         private ISolutionManager _solutionManager;
 
+        private JoinableTaskFactory PumpingJTF { get; }
+
         [ImportingConstructor]
         public VsPackageUninstaller(ISourceRepositoryProvider sourceRepositoryProvider, ISettings settings, ISolutionManager solutionManager)
         {
             _sourceRepositoryProvider = sourceRepositoryProvider;
             _settings = settings;
             _solutionManager = solutionManager;
+            PumpingJTF = new PumpingJTF(ThreadHelper.JoinableTaskContext);
         }
 
         public void UninstallPackage(Project project, string packageId, bool removeDependencies)
@@ -41,7 +45,7 @@ namespace NuGet.VisualStudio
                 throw new ArgumentException(String.Format(CultureInfo.CurrentCulture, CommonResources.Argument_Cannot_Be_Null_Or_Empty, "packageId"));
             }
 
-            ThreadHelper.JoinableTaskFactory.Run(async delegate
+            PumpingJTF.Run(async delegate
                 {
                     NuGetPackageManager packageManager = new NuGetPackageManager(_sourceRepositoryProvider, _settings, _solutionManager);
 
