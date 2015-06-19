98253192ecf491db5f87d3fd05e32565b3a227c0
Justin Emgarten <justin@emgarten.com>
2015-06-19 11:34:22 -0700

    3.0.0-beta to 3.0.0

--- Common/CommonAssemblyInfo.cs
+++ Common/CommonAssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 #if !FIXED_ASSEMBLY_VERSION
 
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0-beta")]
+[assembly: AssemblyInformationalVersion("3.0.0")]
 #endif
 
 [assembly: NeutralResourcesLanguage("en-US")]
