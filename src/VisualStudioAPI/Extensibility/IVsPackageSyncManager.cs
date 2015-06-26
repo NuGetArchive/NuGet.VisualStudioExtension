2fc6d0c2cc72e9ef70393f8a42496c1e4eea7f01
Justin Emgarten <justin@emgarten.com>
2015-06-25 19:20:34 -0700

    Interfaces

---
+++ src/VisualStudioAPI/Extensibility/IVsPackageSyncManager.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using EnvDTE;
+
+namespace NuGet.VisualStudio
+{
+    /// <summary>
+    /// Provides support for updating project.json based projects after modifying the project.json file.
+    /// </summary>
+    [ComImport]
+    [Guid("5006BAC9-CCC7-4AD1-85C2-BE0AF6463667")]
+    public interface IVsPackageSyncManager
+    {
+        /// <summary>
+        /// Restore packages for a project.json based project and generate a project.lock.json file.
+        /// </summary>
+        /// <param name="project">DTE project to update.</param>
+        /// <param name="sources">Source information used to find packages.</param>
+        /// <param name="token">Cancel token</param>
+        /// <returns>Returns the operation status.</returns>
+        Task<IVsPackageSyncResult> UpdateProjectAync(Project project, IVsPackageSourceContext sources, CancellationToken token);
+    }
+}
---
+++ src/VisualStudioAPI/Extensibility/IVsPackageSyncResult.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace NuGet.VisualStudio
+{
+    /// <summary>
+    /// Package restore and update operation result for project.json based projects.
+    /// </summary>
+    [ComImport]
+    [Guid("758AE2F5-A72D-4D4B-9FF7-8324B874BF89")]
+    public interface IVsPackageSyncResult
+    {
+        /// <summary>
+        /// True if the operation completed successfully.
+        /// </summary>
+        bool Success { get; }
+    }
+}
