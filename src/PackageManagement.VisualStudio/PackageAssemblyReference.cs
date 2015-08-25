a5937c8639ae867193e0b3b66b4f7d6b9a69501e
Zhi Li <zhili128@outlook.com>
2015-08-25 15:15:49 -0700

    fixed https://github.com/NuGet/Home/issues/1245

---
+++ src/PackageManagement.VisualStudio/PackageAssemblyReference.cs
@@ -0,0 +1,20 @@
+﻿namespace NuGet.PackageManagement.VisualStudio
+{
+    internal class PackageAssemblyReference : IPackageAssemblyReference
+    {
+        private string _path;
+
+        public PackageAssemblyReference(string path)
+        {
+            _path = path;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return System.IO.Path.GetFileName(_path);
+            }
+        }
+    }
+}
--- src/PackageManagement.VisualStudio/ScriptPackage.cs
+++ src/PackageManagement.VisualStudio/ScriptPackage.cs
@@ -1,6 +1,12 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NuGet.Packaging;
+
 namespace NuGet.PackageManagement.VisualStudio
 {
     // this scriptPackage is a package inferface for script executor
@@ -9,11 +15,16 @@ namespace NuGet.PackageManagement.VisualStudio
     {
         private string _id;
         private string _version;
+        private string _installPath;
+        private IList<IPackageAssemblyReference> _assemblyReferences;
+        private const string ResourceAssemblyExtension = ".resources.dll";
+        private const string PackageEmptyFileName = "_._";
 
-        public ScriptPackage(string id, string version)
+        public ScriptPackage(string id, string version, string installPath)
         {
             _id = id;
             _version = version;
+            _installPath = installPath;
         }
 
         public string Id
@@ -24,6 +35,49 @@ namespace NuGet.PackageManagement.VisualStudio
         public string Version
         {
             get { return _version; }
+        }
+
+        public IEnumerable<IPackageAssemblyReference> AssemblyReferences
+        {
+            get
+            {
+                if (_assemblyReferences == null)
+                {
+                    _assemblyReferences = GetAssemblyReferencesCore().ToList();
+                }
+
+                return _assemblyReferences;
+            }
+        }
+
+        private IEnumerable<IPackageAssemblyReference> GetAssemblyReferencesCore()
+        {
+            var reader = new PackageFolderReader(_installPath);
+           
+            return (from file in reader.GetFiles()
+                    where IsAssemblyReference(file)
+                    select (IPackageAssemblyReference)new PackageAssemblyReference(file)).ToList();
+        }
+
+        private bool IsAssemblyReference(string filePath)
+        {
+            // assembly reference must be under lib/
+            if (!filePath.StartsWith("lib" + Path.AltDirectorySeparatorChar, StringComparison.OrdinalIgnoreCase))
+            {
+                return false;
+            }
+
+            var fileName = Path.GetFileName(filePath);
+
+            // if it's an empty folder, yes
+            if (fileName == PackageEmptyFileName)
+            {
+                return true;
+            }
+
+            // Assembly reference must have a .dll|.exe|.winmd extension and is not a resource assembly;
+            return !filePath.EndsWith(ResourceAssemblyExtension, StringComparison.OrdinalIgnoreCase) &&
+                Constants.AssemblyReferencesExtensions.Contains(Path.GetExtension(filePath), StringComparer.OrdinalIgnoreCase);
         }
     }
 }
