2bff3dcce4f4c107d94dcb7a98e722a814df8f2c
Dan Liu <danliu@microsoft.com>
2015-09-23 16:29:37 -0700

    Add tests

---
+++ test/EndToEnd/tests/ProjectJsonSchema.ps1
@@ -0,0 +1,32 @@
+function Test-BuildIntegratedSchemaUrl {
+    # Arrange
+    $p = New-BuildIntegratedProj UAPApp
+
+    # Act
+	$componentModel = Get-VSComponentModel
+    $schemaSelector = $componentModel.GetService([Microsoft.JSON.Core.Schema.IJSONSchemaSelector])
+	Write-Host $schemaSelector	
+	Assert-NotNull $schemaSelector "selector"
+	$item = Get-ProjectItem $p project.json
+	Write-Host $item
+	Assert-NotNull $item "item"
+	$schemaUrl = $schemaSelector.GetSchemaFor($item)
+
+    # Assert
+	Assert-NotNull $schemaUrl
+	Assert-AreEqual($schemaUrl, "http://json.schemastore.org/nuget-project")
+}
+
+function Test-LegacyProjectSchemaUrl {
+    # Arrange
+    $p = New-ClassLibrary
+
+    # Act
+	$componentModel = Get-VSComponentModel
+    $schemaSelector = $componentModel.GetService([Microsoft.JSON.Core.Schema.IJSONSchemaSelector])
+	$item = Get-ProjectItem $p packages.config
+	$schemaUrl = $schemaSelector.GetSchemaFor($item)
+
+    # Assert
+	Assert-Null $schemaUrl
+}
