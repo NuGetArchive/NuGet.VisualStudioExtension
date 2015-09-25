aea78fce2c66bc57354e8deb9256923da44f7fa8
Dan Liu <danliu@microsoft.com>
2015-09-25 12:00:57 -0700

    add automated tests

--- test/EndToEnd/tests/ProjectJsonSchema.ps1
+++ test/EndToEnd/tests/ProjectJsonSchema.ps1
@@ -3,33 +3,39 @@ function Test-BuildIntegratedSchemaUrl {
     $p = New-BuildIntegratedProj UAPApp
 
     # Act
-	#$componentModel = Get-VSComponentModel
-    #$schemaSelector = $componentModel.GetService([Microsoft.JSON.Core.Schema.IJSONSchemaSelector])
+	$componentModel = Get-VSComponentModel
+	# Multiple exports can be found for this interface.
+    $schemaSelectors = $componentModel.GetExtensions([Microsoft.JSON.Core.Schema.IJSONSchemaSelector])
 
-	$schemaSelector = [NuGet.PackageManagement.VisualStudio.ServiceLocator]::GetInstanceSafe([IJSONSchemaSelector])
-
-	Write-Host $schemaSelector	
-	Assert-NotNull $schemaSelector "selector"
 	$item = Get-ProjectItem $p project.json
-	Write-Host $item
-	Assert-NotNull $item "item"
-	$schemaUrl = $schemaSelector.GetSchemaFor($item)
+	$path = $item.Properties.Item("FullPath").Value
+
+	# NuGet schema selector is called following a fixed order
+	$nugetSelector = $schemaSelectors[3]
+	$schemaUrl = $nugetSelector.GetSchemaFor($path)
 
     # Assert
+	Assert-AreEqual $nugetSelector.ToString() NuGet.VisualStudio.NuGetProjectJSONSchemaSelector
 	Assert-NotNull $schemaUrl
-	Assert-AreEqual($schemaUrl, "http://json.schemastore.org/nuget-project")
+	Assert-AreEqual $schemaUrl 'http://json.schemastore.org/nuget-project'
 }
 
-function LegacyProjectSchemaUrl {
+function Test-LegacyProjectSchemaUrl {
     # Arrange
     $p = New-ClassLibrary
+	Install-Package jQuery -version 2.1.4
 
     # Act
 	$componentModel = Get-VSComponentModel
-    $schemaSelector = $componentModel.GetService([Microsoft.JSON.Core.Schema.IJSONSchemaSelector])
+    $schemaSelectors = $componentModel.GetExtensions([Microsoft.JSON.Core.Schema.IJSONSchemaSelector])
+
 	$item = Get-ProjectItem $p packages.config
-	$schemaUrl = $schemaSelector.GetSchemaFor($item)
+	$path = $item.Properties.Item("FullPath").Value
+
+	$nugetSelector = $schemaSelectors[3]
+	$schemaUrl = $nugetSelector.GetSchemaFor($path)
 
     # Assert
+	Assert-AreEqual $nugetSelector.ToString() NuGet.VisualStudio.NuGetProjectJSONSchemaSelector
 	Assert-Null $schemaUrl
 }
